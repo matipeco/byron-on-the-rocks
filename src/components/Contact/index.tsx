3568a6ca46c4a9e7ec6458ffe65e8eb0365fcb03
matipeco <matiaspecorale1995@gmail.com>
2023-07-15 23:43:54 -0300

    update Contact y NavBar

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -2,7 +2,7 @@ import './style.scss'
 
 export const Contact = () =>{
   return(
-    <section className='contact__container'>
+    <section id='Contact' className='contact__container'>
       Contact
     </section>
   )
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,8 +2,13 @@ import './style.scss';
 
 export const NavBar = ()=>{
   return(
-    <nav className='nav'>
-      Navbar
+    <nav>
+      <ul>
+        <li><a href="#Home">Home</a></li>
+        <li><a href="#OurServices">Our Services</a></li>
+        <li><a href="#About">About</a> </li>
+        <li><a href="#Contact">Contact</a></li>
+      </ul>
     </nav>
   )
 }
