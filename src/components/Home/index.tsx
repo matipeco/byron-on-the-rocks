f8c92916fb4d5e83e1e181e2072d6f9e173b9ec6
matipeco <matiaspecorale1995@gmail.com>
2023-07-17 23:54:59 -0300

    update Home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,11 +1,21 @@
-import background from "../../assets/background.jpg"
+import background from "../../assets/edit.png"
+import { Container } from "../Container"
 import './style.scss'
 
-export const Home = ()=>{
+export const Home = ()=> {
   return(
     <header id="Home" className="home__container">
-      
       <img className="home__background" src={background} alt="" width='50' height='50'/>
+      <Container>
+        <div className="home__text">
+          <h2>Welcome to our world of <br/>
+            unique flavors and experiences!</h2>
+          <p>We are <h1>Byron On The Rocks</h1>, experts in <br/>
+           pop-up bar events in the beautiful region <br/>
+          of Northern Rivers and its surroundings.</p>
+        </div>
+      </Container>
+      <div className="home__gradient-overlay "></div>
     </header>
   )
 }
