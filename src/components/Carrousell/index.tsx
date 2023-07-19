262cc2de892f5f292a49d7ad59db758c8aabc1b6
matipeco <matiaspecorale1995@gmail.com>
2023-07-19 17:37:31 -0300

    update Carrousel con Swipper andando

--- src/components/Carrousell/index.tsx
+++ src/components/Carrousell/index.tsx
@@ -1,43 +1,53 @@
-import './styled.scss'
+import './styled.scss';
+import img1 from "../../assets/carrousell/img1.jpg";
+import img2 from "../../assets/carrousell/img2.jpg";
+import img3 from "../../assets/carrousell/img3.jpg";
+import { Container } from '../Container';
 
-import img1 from "../../assets/carrousell/img1.jpg"
-import img2 from "../../assets/carrousell/img2.jpg"
-import img3 from "../../assets/carrousell/img3.jpg"
-// import { Container } from '../Container'
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectFade } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-export const Carrousell =()=>{
-  return(
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/effect-fade'; // Importa el CSS del efecto de "fading"
+
+
+export const Carrousell = () => {
+
+  //cambiar color de la paginacion segun posicion
+  return (
     <section className='container__carrousell'>
-      <div className='slider'>
-        <input type="radio" id='radio1' name='image-slide' />
-        <input type="radio" id='radio2' name='image-slide' />
-        <input type="radio" id='radio3' name='image-slide' />
-
-          <div className='slide first'>
+      <Container>
+        <Swiper
+          className='container__slides'
+          modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectFade]}
+          effect="fade" // Agrega la opción 'effect' con el valor 'fade'
+          spaceBetween={50}
+          slidesPerView={1}
+          navigation={false}
+          pagination={{ clickable: true }}
+          autoplay={{
+            delay:800,
+            disableOnInteraction: false,
+          }}
+          speed={1000}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log('slide change')}
+        >
+          <SwiperSlide className='slide'>
             <img className='' src={img3} alt="" width="800" height="500"/>
-          </div>
-
-          <div className='slide'>
-            <img className='' src={img2} alt="" width="800" height="500"/>
-          </div>
-
-          <div className='slide'>
+          </SwiperSlide>
+          <SwiperSlide className='slide'>
             <img className='' src={img1} alt="" width="800" height="500"/>
-          </div>
-
-        <div className='nav-auto'>
-          <div className='auto-btn1'></div>
-          <div className='auto-btn2'></div>
-          <div className='auto-btn3'></div>
-        </div>
-
-        <div className='nav-manual'>
-          <label className='manual-btn' htmlFor="radio1"></label>
-          <label className='manual-btn' htmlFor="radio2"></label>
-          <label className='manual-btn' htmlFor="radio3"></label>
-        </div>
-      </div>
+          </SwiperSlide>
+          <SwiperSlide className='slide'>
+            <img className='' src={img2} alt="" width="800" height="500"/>
+          </SwiperSlide>
+        </Swiper>
+      </Container>
     </section>
-
   )
 }
