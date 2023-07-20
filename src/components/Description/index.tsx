5868571f8b9bb3595caa8a81d3500b932c0731a7
matipeco <matiaspecorale1995@gmail.com>
2023-07-20 19:00:52 -0300

    update Description

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -1,9 +1,22 @@
+import { Container } from '../Container'
+import services from "../../assets/carrousell/img1.jpg"
 import './styled.scss'
 
 export const Description =()=>{
   return(
-    <section className='description__container'>
-      <p>Description</p>
+    <section id="Description" className='description__container'>
+      <Container>
+        {/* <p>
+          Our talented bartenders specialize in bar catering service in all kind of events; ranging from weddings, birthdays celebrations, corporate events, or any other private gathering.
+        </p> */}
+          <p className='description__text'>Our talented bartenders specialize in bar catering service in all kind of events; ranging from weddings, birthdays celebrations, corporate events, or any other private gathering.</p>
+        <div  className='description__box'>
+          <img src={services} alt="" width="50" height="50" />
+          <p>
+          With much love and dedication, we will adapt our service to match your preferences, ensuring that your vision becomes a reality.
+        </p>
+        </div>
+      </Container>
     </section>
   )
 }
