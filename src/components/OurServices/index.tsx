cc3b28f05c9801972c9ddccd0ef2be06f40cea88
matipeco <matiaspecorale1995@gmail.com>
2023-07-24 22:05:32 -0300

    our Services update

--- src/components/OurServices/index.tsx
+++ src/components/OurServices/index.tsx
@@ -3,7 +3,12 @@ import './style.scss'
 export const OurServices = ()=>{
   return(
     <section id='OurServices' className='ourservices__container'>
-      Our Services
+      <p>
+      At Byron On The Rocks, we take pride in offering customized beverage packages that suit any venue.
+      </p>
+      <p>
+        
+      </p>
     </section>
   )
 }
