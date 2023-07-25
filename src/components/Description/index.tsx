1b97730e5b9ba287ede60d9676590cd9d7f7a879
matipeco <matiaspecorale1995@gmail.com>
2023-07-25 11:03:01 -0300

    update Description

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -1,30 +1,15 @@
-import services from "../../assets/carrousell/img1.jpg"
 import './styled.scss'
+import especias from '../../assets/Fondo especias.png'
 
 export const Description =()=>{
   return(
-    <>
     <section className='description__container'>
-        {/* <p>
-          Our talented bartenders specialize in bar catering service in all kind of events; ranging from weddings, birthdays celebrations, corporate events, or any other private gathering.
-        </p> */}
-        <div className='description__container-text'>
-          <p className='description__text1'>Our talented bartenders specialize in </p>
-          <span>bar catering service in all kind of events</span>
-          <p className='description__text2'>
-          ranging from weddings, birthdays celebrations, corporate events, or any other private gathering.
-          </p>
-        </div>
-          
+      <img src={especias} alt="" width='50' height='50'/>
+      <p className='description__text1'>Our talented bartenders specialize in </p>
+      <span>bar catering service in all kind of events</span>
+      <p className='description__text2'>
+        ranging from weddings, birthdays celebrations, corporate events, or any other private gathering.
+      </p>
     </section>
-    <div  className='description__box'>
-          <img src={services} alt="" width="50" height="50" />
-          <p>
-          With much love and dedication, we will adapt our service to match your preferences, ensuring that your vision becomes a reality.
-          </p>
-        </div>
-    </>
   )
-    
-        
 }
