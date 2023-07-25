b05c8302db07fd8aba27dc1e3aa438644b7087a9
matipeco <matiaspecorale1995@gmail.com>
2023-07-24 22:06:01 -0300

    agregando capa description y carrousel update

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -1,22 +1,30 @@
-import { Container } from '../Container'
 import services from "../../assets/carrousell/img1.jpg"
 import './styled.scss'
 
 export const Description =()=>{
   return(
-    <section id="Description" className='description__container'>
-      <Container>
+    <>
+    <section className='description__container'>
         {/* <p>
           Our talented bartenders specialize in bar catering service in all kind of events; ranging from weddings, birthdays celebrations, corporate events, or any other private gathering.
         </p> */}
-          <p className='description__text'>Our talented bartenders specialize in bar catering service in all kind of events; ranging from weddings, birthdays celebrations, corporate events, or any other private gathering.</p>
-        <div  className='description__box'>
+        <div className='description__container-text'>
+          <p className='description__text1'>Our talented bartenders specialize in </p>
+          <span>bar catering service in all kind of events</span>
+          <p className='description__text2'>
+          ranging from weddings, birthdays celebrations, corporate events, or any other private gathering.
+          </p>
+        </div>
+          
+    </section>
+    <div  className='description__box'>
           <img src={services} alt="" width="50" height="50" />
           <p>
           With much love and dedication, we will adapt our service to match your preferences, ensuring that your vision becomes a reality.
-        </p>
+          </p>
         </div>
-      </Container>
-    </section>
+    </>
   )
+    
+        
 }
