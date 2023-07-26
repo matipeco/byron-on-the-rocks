f400c7e725dee003dc7631fac640ebe157ef36d2
matipeco <matiaspecorale1995@gmail.com>
2023-07-26 17:16:39 -0300

    completando componente Contact

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,9 +1,18 @@
+import { Container } from '../Container'
+import { FormEmail } from '../FormEmail'
 import './style.scss'
 
 export const Contact = () =>{
   return(
     <section id='Contact' className='contact__container'>
-      Contact
+      <Container>
+        <div className='contact__container-text'>
+          <h3>Contact Us</h3>
+          <p>Phone: +61 431 218 161</p>
+          <p>Email: byronontherocksbar@gmail.com</p>
+        </div>
+        <FormEmail/>
+      </Container>
     </section>
   )
 }
