6f7688422f8d76d84fa27102e0cf885827aaaff8
matipeco <matiaspecorale1995@gmail.com>
2023-07-26 17:16:04 -0300

    componente About y agregando img

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,9 +1,29 @@
+import { Container } from '../Container'
 import './style.scss'
 
 export const About = () =>{
   return(
     <section id='About' className='about__container'>
-      About
+      <Container>
+        <div className='about__container-text'>
+          <h3 className='about__title'>About Us</h3>
+          <p>
+            My name is <span>Pedro</span>. For many years I've been fascinated by the world of drinks and mixology.
+          </p>
+          <p>
+            At 27 years old, I decided to venture into bartending, dive in headfirst, and start my own bar catering service. I love surprising people with different combinations and flavors.
+          </p>
+          <p>
+          The possibility of bringing my ideas to different events and venues excites me. <span>I want to make each occasion unique</span> and ensure people enjoy special moments with a drink in hand.
+          </p>
+          <p>
+            <span>Byron On The Rocks</span> is not just a job for me; it's a lifestyle filled with joy, creativity, and dedication.
+          </p>
+          <span>
+            Cheers!
+          </span>
+        </div>
+      </Container>
     </section>
   )
 }
