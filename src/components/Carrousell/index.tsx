4a822f98570850f1a02dc3ca69db871349b102be
matipeco <matiaspecorale1995@gmail.com>
2023-07-25 21:19:38 -0300

    update componente Carrousell, Description, home

--- src/components/Carrousell/index.tsx
+++ src/components/Carrousell/index.tsx
@@ -2,52 +2,71 @@ import './styled.scss';
 import img1 from "../../assets/carrousell/img1.jpg";
 import img2 from "../../assets/carrousell/img2.jpg";
 import img3 from "../../assets/carrousell/img3.jpg";
-import { Container } from '../Container';
-
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectFade } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import 'swiper/css/effect-fade'; // Importa el CSS del efecto de "fading"
+import img4 from "../../assets/carrousell/img4.jpg";
+import img5 from "../../assets/carrousell/img5.jpg";
+import img6 from "../../assets/carrousell/img6.jpg";
+import img7 from "../../assets/carrousell/img7.jpg";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/splide.min.css';
+// import '@splidejs/react-splide/css/skyblue';
+// import '@splidejs/react-splide/css/sea-greens';
 
 
 export const Carrousell = () => {
 
-  //cambiar color de la paginacion segun posicion
   return (
     <section className='container__carrousell'>
-      <Container>
-        <Swiper
-          className='container__slides'
-          modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectFade]}
-          effect="fade" // Agrega la opción 'effect' con el valor 'fade'
-          spaceBetween={50}
-          slidesPerView={1}
-          navigation={false}
-          pagination={{ clickable: true }}
-          autoplay={{
-            delay:800,
-            disableOnInteraction: false,
-          }}
-          speed={1000}
-          onSwiper={(swiper) => console.log(swiper)}
-          onSlideChange={() => console.log('slide change')}
+      <Splide 
+        options={{
+          type: 'loop',
+          perPage: 3, 
+          focus  : 'center',
+          fixedWidth: '800px',
+          autoplay: true,
+          interval: 2200,
+          trimSpace: false,
+          drag: true,
+          easing: 'cubic-bezier(0, 0, 0.58, 1)',
+          speed:350,
+          rewind: true,
+          gap: 180
+        }}
         >
-          <SwiperSlide className='slide'>
-            <img className='' src={img3} alt="" width="800" height="500"/>
-          </SwiperSlide>
-          <SwiperSlide className='slide'>
-            <img className='' src={img1} alt="" width="800" height="500"/>
-          </SwiperSlide>
-          <SwiperSlide className='slide'>
-            <img className='' src={img2} alt="" width="800" height="500"/>
-          </SwiperSlide>
-        </Swiper>
-      </Container>
+        <SplideSlide>
+          <img className='slide-img' src={img1} alt="" width="800" height="500"/>
+
+        </SplideSlide>
+        <SplideSlide>
+          <img className='slide-img' src={img2} alt="" width="800" height="500"/>
+
+        </SplideSlide>
+        <SplideSlide>
+          <img className='slide-img' src={img3} alt="" width="800" height="500"/>
+
+        </SplideSlide>
+        <SplideSlide>
+          <img className='slide-img' src={img4} alt="" width="800" height="500"/>
+
+        </SplideSlide>
+      
+        <SplideSlide>
+          <img className='slide-img' src={img5} alt="" width="800" height="500"/>
+
+        </SplideSlide>
+        <SplideSlide>
+          <img className='slide-img' src={img6} alt="" width="800" height="500"/>
+
+        </SplideSlide>
+      
+        <SplideSlide>
+          <img className='slide-img' src={img7} alt="" width="800" height="500"/>
+
+        </SplideSlide>
+      
+      </Splide>
+          
     </section>
   )
 }
--- src/components/OurServices/index.tsx
+++ src/components/OurServices/index.tsx
@@ -3,12 +3,12 @@ import './style.scss'
 export const OurServices = ()=>{
   return(
     <section id='OurServices' className='ourservices__container'>
-      <p>
+      {/* <p>
       At Byron On The Rocks, we take pride in offering customized beverage packages that suit any venue.
-      </p>
-      <p>
+      </p> */}
+      {/* <p>
         
-      </p>
+      </p> */}
     </section>
   )
 }
