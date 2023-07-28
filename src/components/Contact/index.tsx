d2d86e9ccfd90841f3661e73137d4c98060d539c
matipeco <matiaspecorale1995@gmail.com>
2023-07-28 18:19:00 -0300

    update Contact y OurServices

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,18 +1,26 @@
-import { Container } from '../Container'
-import { FormEmail } from '../FormEmail'
-import './style.scss'
+import { Container } from "../Container";
+import { FormEmail } from "../FormEmail";
+import vaso from "../../assets/vaso.svg";
+import "./style.scss";
 
-export const Contact = () =>{
-  return(
-    <section id='Contact' className='contact__container'>
+export const Contact = () => {
+  return (
+    <section id="Contact" className="contact__container">
       <Container>
-        <div className='contact__container-text'>
+        <img
+          className="contact__img"
+          src={vaso}
+          alt=""
+          width="50"
+          height="50"
+        />
+        <div className="contact__container-text">
           <h3>Contact Us</h3>
           <p>Phone: +61 431 218 161</p>
           <p>Email: byronontherocksbar@gmail.com</p>
         </div>
-        <FormEmail/>
+        <FormEmail />
       </Container>
     </section>
-  )
-}
+  );
+};
