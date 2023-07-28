5e15c6a7781a4cd27a7226219b7c3a604f5af86c
matipeco <matiaspecorale1995@gmail.com>
2023-07-28 18:18:12 -0300

    update carrousell

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import { About } from "./components/About"
-import { BoxDescription } from "./components/BoxDescription"
-import { Carrousell } from "./components/Carrousell"
-import { Contact } from "./components/Contact"
-import { Description } from "./components/Description"
-import { Footer } from "./components/Footer"
-import { Home } from "./components/Home"
-import { NavBar } from "./components/NavBar"
-import { OurServices } from "./components/OurServices"
+import { About } from "./components/About";
+import { BoxDescription } from "./components/BoxDescription";
+import { Carrousell } from "./components/Carrousell";
+import { Contact } from "./components/Contact";
+import { Description } from "./components/Description";
+import { Footer } from "./components/Footer";
+import { Home } from "./components/Home";
+import { NavBar } from "./components/NavBar";
+import { OurServices } from "./components/OurServices";
 
 function App() {
   return (
     <>
       <main>
-          <NavBar/>
-          <Home/>
-          <Description/>
-          <BoxDescription/>
-          <Carrousell/>
-          <OurServices/>
-          <About/>
-          <Contact/>
+        <NavBar />
+        <Home />
+        <Description />
+        <BoxDescription />
+        <Carrousell />
+        <OurServices />
+        <About />
+        <Contact />
       </main>
-      <Footer/>
+      {/* <Footer/> */}
     </>
-  )
+  );
 }
 
 export default App;
--- src/components/Carrousell/index.tsx
+++ src/components/Carrousell/index.tsx
@@ -1,4 +1,5 @@
-import './styled.scss';
+import "./styled.scss";
+import vasito from "../../assets/vasito.png";
 import img1 from "../../assets/carrousell/img1.jpg";
 import img2 from "../../assets/carrousell/img2.jpg";
 import img3 from "../../assets/carrousell/img3.jpg";
@@ -8,65 +9,106 @@ import img6 from "../../assets/carrousell/img6.jpg";
 import img7 from "../../assets/carrousell/img7.jpg";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-ignore
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import '@splidejs/splide/dist/css/splide.min.css';
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
 // import '@splidejs/react-splide/css/skyblue';
 // import '@splidejs/react-splide/css/sea-greens';
 
-
 export const Carrousell = () => {
-
   return (
-    <section className='container__carrousell'>
-      <Splide 
+    <section className="container__carrousell">
+      <img
+        className="carrousell__img"
+        src={vasito}
+        alt=""
+        width="1920"
+        height="1080"
+      />
+      <Splide
         options={{
-          type: 'loop',
-          perPage: 3, 
-          focus  : 'center',
-          fixedWidth: '800px',
+          type: "loop",
+          perPage: 2,
+          focus: "center",
+          // width: "100%",
+          // height: "50rem",
+          // fixedWidth: "auto",
+          // fixedHeight: "40rem",
           autoplay: true,
           interval: 2200,
           trimSpace: false,
           drag: true,
-          easing: 'cubic-bezier(0, 0, 0.58, 1)',
-          speed:350,
+          easing: "cubic-bezier(0, 0, 0.58, 1)",
+          speed: 350,
           rewind: true,
-          gap: 180
+          // gap: 180,
         }}
-        >
+      >
         <SplideSlide>
-          <img className='slide-img' src={img1} alt="" width="800" height="500"/>
-
+          <img
+            className="slide-img"
+            src={img1}
+            alt=""
+            width="800"
+            height="500"
+          />
         </SplideSlide>
         <SplideSlide>
-          <img className='slide-img' src={img2} alt="" width="800" height="500"/>
-
+          <img
+            className="slide-img"
+            src={img2}
+            alt=""
+            width="800"
+            height="500"
+          />
         </SplideSlide>
         <SplideSlide>
-          <img className='slide-img' src={img3} alt="" width="800" height="500"/>
-
+          <img
+            className="slide-img"
+            src={img3}
+            alt=""
+            width="800"
+            height="500"
+          />
         </SplideSlide>
         <SplideSlide>
-          <img className='slide-img' src={img4} alt="" width="800" height="500"/>
-
+          <img
+            className="slide-img"
+            src={img4}
+            alt=""
+            width="800"
+            height="500"
+          />
         </SplideSlide>
-      
+
         <SplideSlide>
-          <img className='slide-img' src={img5} alt="" width="800" height="500"/>
-
+          <img
+            className="slide-img"
+            src={img5}
+            alt=""
+            width="800"
+            height="500"
+          />
         </SplideSlide>
         <SplideSlide>
-          <img className='slide-img' src={img6} alt="" width="800" height="500"/>
-
+          <img
+            className="slide-img"
+            src={img6}
+            alt=""
+            width="800"
+            height="500"
+          />
         </SplideSlide>
-      
+
         <SplideSlide>
-          <img className='slide-img' src={img7} alt="" width="800" height="500"/>
-
+          <img
+            className="slide-img"
+            src={img7}
+            alt=""
+            width="800"
+            height="500"
+          />
         </SplideSlide>
-      
       </Splide>
-          
     </section>
-  )
-}
+  );
+};
