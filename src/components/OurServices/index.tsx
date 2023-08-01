557659d4a085ff26bf9d262b9100add35e870fbe
matipeco <matiaspecorale1995@gmail.com>
2023-08-01 17:18:27 -0300

    añadiendo contenido OurServices

--- src/components/OurServices/index.tsx
+++ src/components/OurServices/index.tsx
@@ -1,14 +1,24 @@
-import './style.scss'
+import "./style.scss";
 
-export const OurServices = ()=>{
-  return(
-    <section id='OurServices' className='ourservices__container'>
-      {/* <p>
-      At Byron On The Rocks, we take pride in offering customized beverage packages that suit any venue.
-      </p> */}
-      {/* <p>
-        
-      </p> */}
+export const OurServices = () => {
+  return (
+    <section id="OurServices" className="ourservices__container">
+      <p>
+        At Byron On The Rocks, we take pride in offering customized beverage
+        packages that suit any venue.
+      </p>
+      <p>
+        From simple packages featuring a selection of beers, wines, and
+        champagnes; to delving into the world of expertly crafted cocktails,
+        mocktails, and homemade lemonade. We have the perfect beverage options
+        to satisfy every palate.
+      </p>
+      <p>
+        Whether you envision an intimate soirée or a grand celebration, let
+        Byron On The Rocks transform your event into an unforgettable
+        experience. Contact us to discuss your event preferences, and let us
+        raise the bar together. Cheers to creating extraordinary moments!
+      </p>
     </section>
-  )
-}
+  );
+};
