f579f40c66e37aa863c92d1af1d7c0510ebe82f5
matipeco <matiaspecorale1995@gmail.com>
2023-08-01 17:19:50 -0300

    añadiendo transiciones a componente Description y Home

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -1,15 +1,24 @@
-import './styled.scss'
-import especias from '../../assets/Fondo especias.png'
+import "./styled.scss";
+import especias from "../../assets/Fondo especias.png";
+import { Fade } from "react-awesome-reveal";
 
-export const Description =()=>{
-  return(
-    <section className='description__container'>
-      <img src={especias} alt="" width='50' height='50'/>
-      <p className='description__text1'>Our talented bartenders specialize in </p>
-      <span>bar catering service in all kind of events</span>
-      <p className='description__text2'>
-        ranging from weddings, birthdays celebrations, corporate events, or any other private gathering.
-      </p>
+export const Description = () => {
+  return (
+    <section className="description__container">
+      <img src={especias} alt="" width="50" height="50" />
+
+      <div className="description__text">
+        <Fade cascade delay={200} damping={0.2} duration={1500} triggerOnce>
+          <p className="description__text1">
+            Our talented bartenders specialize in{" "}
+          </p>
+          <span>bar catering service in all kind of events</span>
+          <p className="description__text2">
+            ranging from weddings, birthdays celebrations, corporate events, or
+            any other private gathering.
+          </p>
+        </Fade>
+      </div>
     </section>
-  )
-}
+  );
+};
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,22 +1,31 @@
 // import background from "../../assets/edit.png"
-import { Container } from "../Container"
-import './style.scss'
+import { Container } from "../Container";
+import "./style.scss";
+import { Fade } from "react-awesome-reveal";
 
-export const Home = ()=> {
-  return(
+export const Home = () => {
+  return (
     <header id="Home" className="home__container">
       <Container>
         <div className="home__text">
-          <h2>Welcome to our world of <br/>
-            unique flavors and experiences!</h2>
-          <p>We are <h1>Byron On The Rocks</h1>, experts in <br/>
-           pop-up bar events in the beautiful region <br/>
-          of Northern Rivers and its surroundings.</p>
-          <div className="scrollAuto" id="Description"></div>
+          <Fade cascade delay={400} damping={0.2} duration={1500} triggerOnce>
+            <h2>
+              Welcome to our world of <br />
+              unique flavors and experiences!
+            </h2>
+            <p>
+              We are <h1>Byron On The Rocks</h1>, experts in <br />
+              pop-up bar events in the beautiful region <br />
+              of Northern Rivers and its surroundings.
+            </p>
+            <div className="scrollAuto" id="Description"></div>
+          </Fade>
         </div>
 
-        <a className="home__button" href="#Description">Explore</a>
+        <a className="home__button" href="#Description">
+          Explore
+        </a>
       </Container>
     </header>
-  )
-}
+  );
+};
