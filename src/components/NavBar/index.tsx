3f7dbf6c63fb4f3b60e44c7879bf5b149809f131
matipeco <matiaspecorale1995@gmail.com>
2023-08-01 17:20:09 -0300

    añadiendo transicion NAvBar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react';
-import { Container } from '../Container';
-import logo from "../../assets/logo-black.svg"
-import './style.scss';
+import { useState, useEffect } from "react";
+import { Container } from "../Container";
+import logo from "../../assets/logo-black.svg";
+import "./style.scss";
+import { Fade } from "react-awesome-reveal";
 
 export const NavBar = () => {
   const [logoSize, setLogoSize] = useState(160);
@@ -15,30 +16,40 @@ export const NavBar = () => {
       setScrolledDown(scrollTop > 0);
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-    return () => window.removeEventListener('scroll', handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
-    <nav className={scrolledDown ? 'scrolled' : ''}>
+    <nav className={scrolledDown ? "scrolled" : ""}>
       <Container>
-        <a href="#">
-          <img
-            className="logo"
-            src={logo}
-            alt="logo"
-            width={logoSize}
-            height={logoSize}
-          />
-        </a>
-        <ul>
-          <li><a href="#Home">Home</a></li>
-          <li><a href="#OurServices">Our Services</a></li>
-          <li><a href="#About">About</a></li>
-          <li><a href="#Contact">Contact</a></li>
-        </ul>
+        <Fade delay={100} direction="down">
+          <a href="#">
+            <img
+              className="logo"
+              src={logo}
+              alt="logo"
+              width={logoSize}
+              height={logoSize}
+            />
+          </a>
+          <ul>
+            <li>
+              <a href="#Home">Home</a>
+            </li>
+            <li>
+              <a href="#OurServices">Our Services</a>
+            </li>
+            <li>
+              <a href="#About">About</a>
+            </li>
+            <li>
+              <a href="#Contact">Contact</a>
+            </li>
+          </ul>
+        </Fade>
       </Container>
     </nav>
   );
-}
+};
