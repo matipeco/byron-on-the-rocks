8e92f6c2c9b774931518ee5baf99aa372ffe3fb0
matipeco <matiaspecorale1995@gmail.com>
2023-07-31 21:58:25 -0300

    update Contact

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,19 +1,19 @@
 import { Container } from "../Container";
 import { FormEmail } from "../FormEmail";
-import vaso from "../../assets/vaso.svg";
+// import vaso from "../../assets/vaso.svg";
 import "./style.scss";
 
 export const Contact = () => {
   return (
     <section id="Contact" className="contact__container">
       <Container>
-        <img
+        {/* <img
           className="contact__img"
           src={vaso}
           alt=""
           width="50"
           height="50"
-        />
+        /> */}
         <div className="contact__container-text">
           <h3>Contact Us</h3>
           <p>Phone: +61 431 218 161</p>
