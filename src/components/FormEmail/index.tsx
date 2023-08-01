f204fd557a467b30fa6750befe8e57cfd239c505
matipeco <matiaspecorale1995@gmail.com>
2023-07-31 21:58:54 -0300

    update FormEmail z-index

--- src/components/FormEmail/index.tsx
+++ src/components/FormEmail/index.tsx
@@ -1,36 +1,45 @@
 import { useRef } from "react";
-import './styled.scss';
-import emailjs from '@emailjs/browser';
+import "./styled.scss";
+import emailjs from "@emailjs/browser";
 
-export const FormEmail = ()=>{
-  const form = useRef<HTMLFormElement| null>(null);
+export const FormEmail = () => {
+  const form = useRef<HTMLFormElement | null>(null);
 
-  const sendEmail = (e:React.FormEvent<HTMLFormElement>) => {
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (form.current) {
       // Aquí usaremos el método sendForm directamente de emailjs
-      emailjs.sendForm('service_vn2hdpq', 'template_6fb029n', form.current, 'F9NYcVQECJnkEiKcH')
-        .then((result) => {
-          console.log(result.text);
-        }, (error) => {
-          console.log(error.text);
-        });
+      emailjs
+        .sendForm(
+          "service_vn2hdpq",
+          "template_6fb029n",
+          form.current,
+          "F9NYcVQECJnkEiKcH"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
     }
   };
 
-  return(
-    <form ref={form} onSubmit={(e)=>sendEmail(e)} className="form__container">
+  return (
+    <form ref={form} onSubmit={(e) => sendEmail(e)} className="form__container">
       <label htmlFor="name">Name</label>
-      <input type="text" name="user_name" id="name"/>
+      <input type="text" name="user_name" id="name" />
 
       <label htmlFor="email">Email</label>
-      <input type="email" name="user_email" id="email"/>
+      <input type="email" name="user_email" id="email" />
 
       <label htmlFor="message">Message</label>
-      <textarea name="message" id="message"/>
+      <textarea name="message" id="message" />
 
       <input type="submit" value="Send" />
     </form>
-  )
-}
+  );
+};
