71ad45ac7c87ee234a6a5c7581bbc271e83400dd
matipeco <matiaspecorale1995@gmail.com>
2023-08-02 17:53:12 -0300

    update Contact

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,6 +1,9 @@
 import { Container } from "../Container";
 import { FormEmail } from "../FormEmail";
 // import vaso from "../../assets/vaso.svg";
+import wpp from "../../assets/wapp.svg";
+import mail from "../../assets/mail.svg";
+import insta from "../../assets/insta.svg";
 import "./style.scss";
 
 export const Contact = () => {
@@ -16,10 +19,21 @@ export const Contact = () => {
         /> */}
         <div className="contact__container-text">
           <h3>Contact Us</h3>
-          <p>Phone: +61 431 218 161</p>
-          <p>Email: byronontherocksbar@gmail.com</p>
+          <FormEmail />
+          <div className="contact__container-icons">
+            <a href="mailto:byronontherocksbar@gmail.com">
+              <img src={mail} alt="" width="45" height="45" />
+            </a>
+            <a href="#">
+              <img src={wpp} alt="" width="45" height="45" />
+            </a>
+            <a href="#">
+              <img src={insta} alt="" width="50" height="50" />
+            </a>
+          </div>
+          {/* <p>Phone: +61 431 218 161</p>
+          <p>Email: byronontherocksbar@gmail.com</p> */}
         </div>
-        <FormEmail />
       </Container>
     </section>
   );
