2dd79becfd8ff95b594d9b1ae94957178822511c
matipecorale <matiaspecorale@gmail.com>
2023-08-10 18:04:09 -0300

    update OurServices

--- src/components/OurServices/index.tsx
+++ src/components/OurServices/index.tsx
@@ -1,27 +1,33 @@
-import { Container } from "../Container";
 import "./style.scss";
-
+import box1 from "../../assets/box1.jpg";
+import box2 from "../../assets/box2.jpg";
 export const OurServices = () => {
   return (
     <section id="OurServices" className="ourservices__container">
-      <Container>
-        <p>
-          At Byron On The Rocks, we take pride in offering customized beverage
-          packages that suit any venue.
+      <div>
+        <p className="ourservices__container-title">
+          At <span>Byron On The Rocks</span>, we take pride in offering
+          customized beverage packages that suit any venue.
         </p>
-        <p>
-          From simple packages featuring a selection of beers, wines, and
-          champagnes; to delving into the world of expertly crafted cocktails,
-          mocktails, and homemade lemonade. We have the perfect beverage options
-          to satisfy every palate.
-        </p>
-        <p>
-          Whether you envision an intimate soirée or a grand celebration, let
-          Byron On The Rocks transform your event into an unforgettable
-          experience. Contact us to discuss your event preferences, and let us
-          raise the bar together. Cheers to creating extraordinary moments!
-        </p>
-      </Container>
+        <div className="ourservices__container-box1">
+          <p>
+            From simple packages featuring a selection of beers, wines, and
+            champagnes; to delving into the world of expertly crafted cocktails,
+            mocktails, and homemade lemonade. We have the perfect beverage
+            options to satisfy every palate.
+          </p>
+          <img src={box1} alt="" width="500" height="500" />
+        </div>
+        {/* <div className="ourservices__container-box2">
+          <img src={box2} alt="" width="500" height="500" />
+          <p>
+            Whether you envision an intimate soirée or a grand celebration, let
+            Byron On The Rocks transform your event into an unforgettable
+            experience. Contact us to discuss your event preferences, and let us
+            raise the bar together. Cheers to creating extraordinary moments!
+          </p>
+        </div> */}
+      </div>
     </section>
   );
 };
