2b0e6e6fdf50ea843b75532979a26944b8b4b0ac
matipeco <matiaspecorale1995@gmail.com>
2023-08-11 23:05:59 -0300

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BoxDescription } from "./components/BoxDescription";
 import { Carrousell } from "./components/Carrousell";
 import { Contact } from "./components/Contact";
 import { Description } from "./components/Description";
-import { Footer } from "./components/Footer";
+// import { Footer } from "./components/Footer";
 import { Home } from "./components/Home";
 import { NavBar } from "./components/NavBar";
 import { OurServices } from "./components/OurServices";
