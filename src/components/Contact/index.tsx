7879102517f8ddbe63fcf728f389d14c3859d51a
matipeco <matiaspecorale1995@gmail.com>
2023-08-11 23:01:01 -0300

    update Contact

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,40 +1,63 @@
-import { Container } from "../Container";
+import logo from "../../assets/logo-black.svg";
 import { FormEmail } from "../FormEmail";
-// import vaso from "../../assets/vaso.svg";
 import wpp from "../../assets/wapp.svg";
 import mail from "../../assets/mail.svg";
 import insta from "../../assets/insta.svg";
+import vaso from "../../assets/vaso.svg";
 import "./style.scss";
 
 export const Contact = () => {
   return (
     <section id="Contact" className="contact__container">
-      <Container>
-        {/* <img
-          className="contact__img"
-          src={vaso}
-          alt=""
-          width="50"
-          height="50"
-        /> */}
-        <div className="contact__container-text">
-          <h3>Contact Us</h3>
-          <FormEmail />
+      <div className="contact__container-box">
+        <div>
+          <img
+            className="contact__container-img"
+            src={logo}
+            alt=""
+            width="500"
+            height="500"
+          />
           <div className="contact__container-icons">
+            <a href="#">
+              <img
+                className="contact__container-icons-wpp"
+                src={wpp}
+                alt=""
+                width="50"
+                height="50"
+              />
+            </a>
+            <a href="#">
+              <img
+                className="contact__container-icons-insta"
+                src={insta}
+                alt=""
+                width="50"
+                height="50"
+              />
+            </a>
             <a href="mailto:byronontherocksbar@gmail.com">
-              <img src={mail} alt="" width="45" height="45" />
-            </a>
-            <a href="#">
-              <img src={wpp} alt="" width="45" height="45" />
-            </a>
-            <a href="#">
-              <img src={insta} alt="" width="50" height="50" />
+              <img
+                className="contact__container-icons-mail"
+                src={mail}
+                alt=""
+                width="50"
+                height="50"
+              />
             </a>
           </div>
-          {/* <p>Phone: +61 431 218 161</p>
-          <p>Email: byronontherocksbar@gmail.com</p> */}
         </div>
-      </Container>
+        <div>
+          <h3>Contact Us</h3>
+          <FormEmail />
+        </div>
+      </div>
     </section>
   );
 };
+
+{
+  /* <p>Phone: +61 431 218 161</p>
+<p>Email: byronontherocksbar@gmail.com</p> */
+}
