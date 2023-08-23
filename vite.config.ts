66ece19afcdb4e0ffaa8c0ea92238a33977771b2
matipeco <matiaspecorale1995@gmail.com>
2023-08-23 16:04:03 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    target: "es2015", // Cambia esto al objetivo que necesites
+    target: "es2015",
   },
   plugins: [react()],
 });
