27a3c276b21379047e1fef0211f35e807abfaf8f
matipeco <matiaspecorale1995@gmail.com>
2023-08-24 12:18:42 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     target: "es2015",
   },
-  plugins: [
-    react(),
-    legacy({
-      targets: ["defaults"],
-    }),
-  ],
+  plugins: [react()],
 });
