037cbb15927d14e4aa81efcbd74c2b6eb62ed4de
matipeco <matiaspecorale1995@gmail.com>
2023-08-27 23:36:10 -0300

    delete footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BoxDescription } from "./components/BoxDescription";
 import { Carrousell } from "./components/Carrousell";
 import { Contact } from "./components/Contact";
 import { Description } from "./components/Description";
-// import { Footer } from "./components/Footer";
 import { Home } from "./components/Home";
 import { NavBar } from "./components/NavBar";
 import { OurServices } from "./components/OurServices";
@@ -21,7 +20,6 @@ function App() {
         <About />
         <Contact />
       </main>
-      {/* <Footer/> */}
     </>
   );
 }
