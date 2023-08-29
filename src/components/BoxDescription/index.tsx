ba7e63a0b619e72ef8c517313cf5324e8fdaa578
matipeco <matiaspecorale1995@gmail.com>
2023-08-29 18:20:26 -0300

    update Boxdescription

--- src/components/BoxDescription/index.tsx
+++ src/components/BoxDescription/index.tsx
@@ -1,11 +1,15 @@
 import { Fade } from "react-awesome-reveal";
 import services from "../../assets/img6.jpg";
+import services2 from "../../assets/mesa1.png";
+import services3 from "../../assets/mesa2.png";
 import "./styled.scss";
 
 export const BoxDescription = () => {
   return (
     <div className="description__box">
-      <img src={services} alt="" width="50" height="50" />
+      {/* <img src={services} alt="" width="50" height="50" /> */}
+      {/* <img src={services2} alt="" width="50" height="50" /> */}
+      <img src={services3} alt="" width="50" height="50" />
       <Fade direction="right" duration={2000} triggerOnce>
         <p>
           With much love and dedication, we will adapt our service to match your
