cfe4eac94a2b1f71fc1d5748503dae6436803205
matipeco <matiaspecorale1995@gmail.com>
2025-11-04 02:09:07 -0300

    update FINAL

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -8,20 +8,26 @@ export const Home = () => {
       <Container>
         <Fade cascade triggerOnce>
           <div className="home__text">
+            {/* Título Desktop */}
             <h2 className="home__text-title">
               Welcome to our world of unique flavors and experiences!
             </h2>
 
+            {/* Título Mobile */}
             <h2 className="home__text-titleMob">
-              <span> Welcome to our world of </span>
+              <span>Welcome to our world of</span>
               <span>unique flavors and experiences!</span>
             </h2>
-            <h1>
+
+            {/* Descripción principal */}
+            <p className="home__text-description">
               We are <span>Byron On The Rocks</span>, experts in pop-up bar
               events in the beautiful region of Northern Rivers and its
               surroundings.
-            </h1>
+            </p>
+
             <div className="scrollAuto" id="Description"></div>
+
             <a className="home__button" href="#Description">
               Explore
             </a>
